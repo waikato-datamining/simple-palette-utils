8a9c52e6407e0a21e50cb5448e10dd428e4eb372
Peter Reutemann <fracpete@gmail.com>
2025-02-04 17:46:47 +1300

    exposed color_lists method

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=[
         "simple_palette_utils",
     ],
-    version="0.0.2",
+    version="0.0.3",
     author='Peter Reutemann',
     author_email='fracpete@waikato.ac.nz',
     install_requires=[
